d42a7a500cff513ee0222f2b5d94a06b4b2f0dc9
espoirMur <espoir.mur@gmail.com>
2019-05-28 17:06:13 +0200

    WIP refractor get tweet script

--- scripts/twitter_streaming.py
+++ scripts/twitter_streaming.py
@@ -5,6 +5,8 @@ import time
 from tweepy import Stream
 from tweepy.streaming import StreamListener
 from twitter_client import get_twitter_auth
+from datetime import datetime, timedelta
+from threading import Timer
 
 
 class CustomListener(StreamListener):
@@ -12,12 +14,14 @@ class CustomListener(StreamListener):
 
     def __init__(self, fname):
         safe_fname = format_filename(fname)
-        self.outfile = "stream_{}.jsonl".format(safe_fname)
+        now = datetime.now()
+        today = now.strftime("%d-%m-%Y-%H-%M-%S")
+        self.outfile = "stream_{}_{}.jsonl".format(safe_fname, today)
 
     def on_data(self, data):
         try:
             print('fetching data ==')
-            with open(self.outfile, 'a') as f:
+            with open('data/{}'.format(self.outfile), 'a') as f:
                 f.write(data)
                 return True
         except BaseException as e:
@@ -53,9 +57,17 @@ def convert_valid(one_char):
     else:
         return '_'
 
+
+def get_tweets(query_fname, max_time):
+    stop = datetime.now() + max_time
+    while datetime.now() < stop:
+        twitter_stream = Stream(auth, CustomListener(query_fname))
+        twitter_stream.filter(track=query, is_async=True)
+
+
 if __name__ == '__main__':
     query = sys.argv[1:] # list of CLI arguments
     query_fname = ' '.join(query) # string
     auth = get_twitter_auth()
-    twitter_stream = Stream(auth, CustomListener(query_fname))
-    twitter_stream.filter(track=query, is_async=True)
+    get_tweets(query_fname, timedelta(minutes=30))
+    
