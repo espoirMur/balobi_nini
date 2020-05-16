3859debdbe235aee92c2d47e81821b275fe1a718
Espoir Murhabazi <espoir.mur@gmail.com>
2020-05-16 13:12:13 +0200

    - Task Completed : (ci) init airflow
    - Next Task :

--- app/static/word_cloud.js
+++ app/static/word_cloud.js
@@ -1,45 +1,29 @@
 function display(data) {
-  // set the dimensions and margins of the graph
-  var svg = d3
-    .select("#drc_word")
-    .append("svg")
-    .attr("style", "solid 1px black")
-    .attr("width", "100%")
-    .attr("height", "350px");
-
-  var projection = d3.geoMercator().center([45, 55]);
-  var path = d3.geoPath().projection(projection);
-
-  var g = svg.append("g");
-  d3.json(
-    "https://raw.githubusercontent.com/johan/world.geo.json/master/countries/AGO.geo.json",
-    function (error, json) {
-      g.selectAll("path")
-        .data(json.features)
-        .enter()
-        .append("path")
-        .attr("d", path)
-        .style("fill", "red");
-    }
-  );
-  // Constructs a new cloud layout instance. It run an algorithm to find the position of words that suits your requirements
   var layout = d3.layout
     .cloud()
-    .size([width, height])
+    .size([500, 500])
     .words(
       Object.keys(data).map(function (key) {
         return { text: key, size: data[key] };
       })
     )
-    .padding(10)
-    .fontSize(60)
+    .padding(5)
+    .rotate(function () {
+      return ~~(Math.random() * 2) * 90;
+    })
+    .font("Impact")
+    .fontSize(function (d) {
+      return d.size;
+    })
     .on("end", draw);
+
   layout.start();
 
-  // This function takes the output of 'layout' above and draw the words
-  // Better not to touch it. To change parameters, play with the 'layout' variable above
   function draw(words) {
-    svg
+    d3.select("#drc_word")
+      .append("svg")
+      .attr("width", layout.size()[0])
+      .attr("height", layout.size()[1])
       .append("g")
       .attr(
         "transform",
@@ -52,6 +36,7 @@ function display(data) {
       .style("font-size", function (d) {
         return d.size + "px";
       })
+      .style("font-family", "Impact")
       .attr("text-anchor", "middle")
       .attr("transform", function (d) {
         return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
